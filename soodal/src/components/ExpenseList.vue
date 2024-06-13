f2b61d658872287e6ef33fba74d6a52eadbe561f
ZeroKgun <maxgun98@naver.com>
2024-06-13 15:50:48 +0900

    style: AddList Modal style 변경

--- soodal/src/components/ExpenseList.vue
+++ soodal/src/components/ExpenseList.vue
@@ -1,86 +1,150 @@
-<template lang="">
-    <div>
-        <img src='../../images/category_icon.png' alt="" width="20px"/>
-        <select v-model="expenseType">
-            <option value="식비" selected>식비</option>
-            <option value="쇼핑">쇼핑</option>
-            <option value="교통">교통</option>
-            <option value="의료">의료</option>
-            <option value="여가">여가</option>
-            <option value="생활비">생활비</option>
-        </select>
-        <br>
-        <img src='../../images/money_icon.png' alt="" width="20px"/>
-        <input type="text" v-model="expenseAmount" placeholder="지출 금액을 입력하세요" size=50 required>
-        <br>
-        <input type="text" v-model="expenseMemo" placeholder="지출 내역을 입력하세요" size=50>
-        <br>
-        <img src='../../images/calendar_icon.png' alt="" width="20px"/>
-        <input type="date" v-model="expenseDate" required>
-        <img src='../../images/time_icon.png' alt="" width="20px"/>
-        <input type="time" v-model="expenseTime">
-        
-        <br>
-        <button @click="ConfirmBtnSubmitHandler">확인</button>
+<template>
+    <div class="expense-form">
+        <div class="form-group">
+            <!-- <label for="category" class="icon"><i class="fas fa-bookmark"></i></label> -->
+            <img src='../../images/category_icon.png' alt="" width="20px"/>
+            <select id="category" v-model="expenseType">
+                <option value="식비" selected>식비</option>
+                <option value="쇼핑">쇼핑</option>
+                <option value="교통">교통</option>
+                <option value="의료">의료</option>
+                <option value="여가">여가</option>
+                <option value="생활비">생활비</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <!-- <label for="amount" class="icon"><i class="fas fa-dollar-sign"></i></label> -->
+            <img src='../../images/money_icon.png' alt="" width="20px"/>
+            <input type="number" id="amount" v-model="expenseAmount" placeholder="지출 금액을 입력하세요." />
+        </div>
+        <div class="form-group">
+            <!-- <label for="details" class="icon"><i class="fas fa-pencil-alt"></i></label> -->
+            <img src='../../images/memo_icon.png' alt="" width="20px"/>
+            <input type="text" id="details" v-model="expenseMemo" placeholder="지출 내역을 입력하세요." />
+        </div>
+        <div class="form-group">
+            <!-- <label for="date" class="icon"><i class="fas fa-calendar-alt"></i></label> -->
+            <img src='../../images/calendar_icon.png' alt="" width="20px"/>
+            <input type="date" id="date" v-model="expenseDate" />
+            <!-- <label for="time" class="icon"><i class="fas fa-clock"></i></label> -->
+            <img src='../../images/time_icon.png' alt="" width="20px"/>
+            <input type="time" id="time" v-model="expenseTime" />
+        </div>
+        <div class="buttons">
+            <!-- <button @click="cancel" class="cancel-button">취소</button> -->
+            <button class="confirm-button" @click="ConfirmBtnSubmitHandler">확인</button>
+        </div>
     </div>
 </template>
+
 <script>
-import { getCurrentInstance, ref } from 'vue';
-import axios from 'axios';
-export default {
-    name: "ExpenseList",
-    setup() {
-        let input_amount = ref('')
-        const { proxy } = getCurrentInstance();
+    import { getCurrentInstance, ref } from 'vue';
+    import axios from 'axios';
+    export default {
+        name: "ExpenseList",
+        setup() {
+            let input_amount = ref('')
+            const { proxy } = getCurrentInstance();
 
-        // 서버 측으로 보낼 데이터 정리
-        let expenseType = ref('')
-        let expenseMemo = ref('')
-        let expenseAmount = ref('')
-        let expenseDate = ref('')
-        let expenseTime = ref('')
+            // 서버 측으로 보낼 데이터 정리
+            let expenseType = ref('')
+            let expenseMemo = ref('')
+            let expenseAmount = ref('')
+            let expenseDate = ref('')
+            let expenseTime = ref('')
 
-        const ConfirmBtnSubmitHandler = async () => {
-            console.log("버튼 클릭 함수 활성화")
+            const ConfirmBtnSubmitHandler = async () => {
+                console.log("버튼 클릭 함수 활성화")
 
-            //id 값을 가져오기 위함
-            const response = await axios.get("http://localhost:3001/data")
-            const members = response.data;
-            console.log("members.length: ", members.length)
+                //id 값을 가져오기 위함
+                const response = await axios.get("http://localhost:3001/data")
+                const members = response.data;
+                console.log("members.length: ", members.length)
 
-            const newId = members.length > 0 ? members.length + 1 : 0
+                const newId = members.length > 0 ? members.length + 1 : 0
 
-            //전역변수 값 대입
-            const member_id = proxy.$member_id
-            const member_pwd = proxy.$member_pwd
-            const member_name = proxy.$member_name
-            const email = proxy.$email
+                //전역변수 값 대입
+                const member_id = proxy.$member_id
+                const member_pwd = proxy.$member_pwd
+                const member_name = proxy.$member_name
+                const email = proxy.$email
 
-            const url = "http://localhost:3001/data"
-            const data = {
-                id: newId,
-                member_id: member_id,
-                member_pwd: member_pwd,
-                member_name: member_name,
-                email: email,
-                date: expenseDate.value,
-                time: expenseTime.value,
-                type: "지출",
-                category: expenseType.value,
-                amount: expenseAmount.value,
-                memo: expenseMemo.value,
+                const url = "http://localhost:3001/data"
+                const data = {
+                    id: newId,
+                    member_id: member_id,
+                    member_pwd: member_pwd,
+                    member_name: member_name,
+                    email: email,
+                    date: expenseDate.value,
+                    time: expenseTime.value,
+                    type: "지출",
+                    category: expenseType.value,
+                    amount: expenseAmount.value,
+                    memo: expenseMemo.value,
+                }
+
+                try {
+                    const response = await axios.post(url, data, { "Content-Type": "application/json" })
+                    console.log("axios 전송 완료 :", response.data)
+                } catch (err) {
+                    alert(err.response.data)
+                }
             }
 
-            try {
-                const response = await axios.post(url, data, { "Content-Type": "application/json" })
-                console.log("axios 전송 완료 :", response.data)
-            } catch (err) {
-                alert(err.response.data)
-            }
+            return { input_amount, expenseType, expenseMemo, expenseAmount, expenseDate, expenseTime, ConfirmBtnSubmitHandler }
         }
-
-        return { input_amount, expenseType, expenseMemo, expenseAmount, expenseDate, expenseTime, ConfirmBtnSubmitHandler }
     }
-}
 </script>
-<style scoped></style>
+
+<style scoped>
+    .expense-form {
+        width: 300px;
+        margin: 0 auto;
+        font-family: Arial, sans-serif;
+        text-align: center;
+    }
+    
+    .form-group {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 15px;
+        width: 100%;
+    }
+    
+    .icon {
+        width: 30px;
+        text-align: center;
+    }
+    
+    input,
+    select {
+        flex: 1;
+        padding: 10px;
+        margin: 0 10px;
+        font-size: 14px;
+    }
+    
+    .buttons {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+    }
+    
+    .cancel-button,
+    .confirm-button {
+        background-color: #7c5a54;
+        color: white;
+        border: none;
+        padding: 10px 20px;
+        cursor: pointer;
+        width: 45%;
+        font-size: 14px;
+        border-radius: 5px;
+    }
+    
+    .confirm-button {
+        background-color: #fecd2f;
+    }
+    </style>
--- soodal/src/components/ListHeader.vue
+++ soodal/src/components/ListHeader.vue
@@ -1,66 +1,101 @@
 <template lang="">
-    <div>
-        <div>
+    <div class="list-header">
+        <div class="header-left">
             <!--돋보기 버튼 이미지를 클릭하면 필터링 된 테이블 값을 화면에 출력한다-->
-            <img src='../../images/glass_icon.png' alt="돋보기 아이콘" width="23px" @click="SearchClickEvent"/>
-            <select id="dropdown" name="dropdown" v-model="selectedCategory">
+            <img src='../../images/glass_icon.png' alt="돋보기 아이콘" width="23px" class="search-icon"
+                @click="SearchClickEvent" />
+            <select class="filter-select" id="dropdown" name="dropdown" v-model="selectedCategory">
                 <option value="식비" selected>식비</option>
                 <option value="쇼핑">쇼핑</option>
                 <option value="교통">교통</option>
                 <option value="의료">의료</option>
                 <option value="여가">여가</option>
                 <option value="생활비">생활비</option>
-
                 <option value="급여">급여</option>
                 <option value="용돈">용돈</option>
             </select>
         </div>
-        
-
-        <!--모달 팝업창 구현 (새로 리스트 추가)-->
-        <img src="../../images/memo_icon.png" width="23px" @click="openModal" alt="추가 이미지" />
-        <AddList :isVisible="isModalVisible" @close="closeModal">
-            <!-- <p>여기에 모달 내용을 넣으세요.</p> -->
-        </AddList>
+        <div class="header-right">
+            <!--모달 팝업창 구현 (새로 리스트 추가)-->
+            <img src="../../images/memo_icon.png" width="23px" @click="openModal" alt="추가 이미지" />
+            <AddList :isVisible="isModalVisible" @close="closeModal">
+                <!-- <p>여기에 모달 내용을 넣으세요.</p> -->
+            </AddList>
+        </div>
     </div>
 
 </template>
 <script>
-import { onMounted, ref } from 'vue';
-import AddList from './AddList.vue';
-export default {
-    name: "ListHeader",
-    components: {
-        AddList
-    },
-    setup(props, { emit }) {
+    import { onMounted, ref } from 'vue';
+    import AddList from './AddList.vue';
+    export default {
+        name: "ListHeader",
+        components: {
+            AddList
+        },
+        setup(props, { emit }) {
 
-        const selectedCategory = ref('')
+            const selectedCategory = ref('')
 
-        //돋보기 버튼 클릭 시 필터링 기능 
-        const SearchClickEvent = () => {
-            console.log("ListHeader emit : ", selectedCategory.value)
-            emit('search-click', selectedCategory.value)
+            //돋보기 버튼 클릭 시 필터링 기능 
+            const SearchClickEvent = () => {
+                console.log("ListHeader emit : ", selectedCategory.value)
+                emit('search-click', selectedCategory.value)
+            }
+
+            const isModalVisible = ref(false);
+
+            const openModal = () => {
+                isModalVisible.value = true;
+            };
+
+            const closeModal = () => {
+                isModalVisible.value = false;
+            };
+
+            return { onMounted, selectedCategory, SearchClickEvent, isModalVisible, openModal, closeModal }
         }
 
-        const isModalVisible = ref(false);
 
-        const openModal = () => {
-            isModalVisible.value = true;
-        };
-
-        const closeModal = () => {
-            isModalVisible.value = false;
-        };
-
-        return { onMounted, selectedCategory, SearchClickEvent, isModalVisible, openModal, closeModal }
     }
-
-
-}
 </script>
 <style scoped>
-* {
-    text-align: center;
-}
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+    /* 구글 폰트 임포트 */
+    
+    .list-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        margin-bottom: 20px;
+    }
+
+    .header-left {
+        display: flex;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .search-icon {
+        width: 24px;
+        height: 24px;
+        cursor: pointer;
+    }
+
+    .filter-select {
+        padding: 10px;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        font-family: 'Roboto', Arial, sans-serif;
+    }
+
+    .header-right {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+    }
 </style>
